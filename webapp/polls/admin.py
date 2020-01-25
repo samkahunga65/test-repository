84a0d713a87c5d27b7b07894e299fd64134d8530
samkahunga65 <samkahunga65@gmail.com>
2020-01-25 19:46:08 +0300

    added changes to the admin page

--- webapp/polls/admin.py
+++ webapp/polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+admin.site.site_header = 'Russia admin'
+admin.site.site_title = 'Russia polls'
+admin.site.index_header = 'Russia polls, instigated by Putin, The Great'
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields' : ['question_text']}),
+    ('Date Information', {'fields' : ['pub_date'], 'classes' : ['collapse']}),]
+    inlines = [ChoiceInline]
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
+
+
